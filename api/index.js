88211c29707f1b04315afad5ea1afb40f0d63685
omhardaha <omprakashhardaha369@gmail.com>
2022-04-07 17:31:23 +0530

    file uploading done

--- api/index.js
+++ api/index.js
@@ -3,21 +3,26 @@ const fs = require("fs");
 const app = express();
 const multer = require("multer");
 app.use(express.json());
-
-// const upload = multer({ dest: "/uploads" });
+// app.use("/static",express.static('uploads'));
+app.use("/static", express.static("uploads"));
 
 const upload = multer({ dest: "uploads/" });
 
 app.post("/post", upload.single("avatar"), (req, res) => {
 	const fileType = req.file.mimetype.split("/")[1];
 	const compleFileName = req.file.filename + "." + fileType;
+	console.log(req.file);
 	console.log(compleFileName);
-	fs.rename("uploads/" + req.file.filename, compleFileName, (err) => {
-		if (err) {
-			console.log(err);
+	fs.rename(
+		"uploads/" + req.file.filename,
+		"uploads/" + compleFileName,
+		(err) => {
+			if (err) {
+				console.log(err);
+			}
 		}
-	});
-	res.status(200).json(req.body.test);
+	);
+	res.status(200).json(`http://localhost:5000/static/${compleFileName}`);
 });
 app.get("/", (req, res) => {
 	res.status(200).json("Welcome boy");
--- hooks/src/Multer/Multer.jsx
+++ hooks/src/Multer/Multer.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
+import React, { useState } from 'react'
 import axios from 'axios'
 export const Multer = () => {
+    const [file, setFile] = useState()
+    const [image, setImage] = useState()
     const handleClick = async (e) => {
         e.preventDefault();
-        const res = await axios.post("/post", {
-            name: "multer",
-            call: "uploading file"
-        })
+        const formData = new FormData();
+        formData.append("avatar", file);
+        const res = await axios.post("/post", formData)
         console.log(res.data);
+        setImage(res.data)
     }
     return (
         <>
             <div> Multer </div>
             <form action="submit">
-                <input type="file" />
+                <input type="file" onChange={e => setFile(e.target.files[0])} />
                 <button type='submit' onClick={handleClick}>Upload File</button>
             </form>
+            <h1>Uploaded File below </h1>
+            <img src={image} alt="" />
         </>
     )
 }
