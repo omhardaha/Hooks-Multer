768d8b071afbcb8bb3ef5291ffeaabf43856bb18
omhardaha <omprakashhardaha369@gmail.com>
2022-05-10 23:37:57 +0530

    Update mg.cpp

--- hooks/src/mg.cpp
+++ hooks/src/mg.cpp
@@ -1,18 +1,8 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
-
-int fib(int n)
-{
-    if (n == 0 || n == 1)
-    {
-        return n;
-    }
-    return fib(n - 1) + fib(n - 2);
-}
 
 int main()
 {
-    for(int i =0;i<9;i++)
-    cout << fib(i)<<" ";
+
     return 0;
 }
